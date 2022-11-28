eb0f5b267664414ab6d33ac6c94a8d5a533b1c9b
yousef aburadi <yousefaburadi@yahoo.com>
2022-11-28 11:21:08 +0300

    initial commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -12,10 +12,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <h1 className=" text-red-300">hkhkjh</h1>
       </main>
 
-      <footer className={styles.footer}>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
